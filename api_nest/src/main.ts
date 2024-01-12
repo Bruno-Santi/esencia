2269dce5f29fb03a74a3f1192fea634da00b1561
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2024-01-12 16:50:11 -0300

    Update main.ts

--- api_nest/src/main.ts
+++ api_nest/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {SocketIoAdapter} from './SocketIoAdapter'
 
-import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
@@ -22,7 +22,7 @@ async function bootstrap() {
 
 
 
-   app.useWebSocketAdapter(new IoAdapter(app)); 
+   app.useWebSocketAdapter(new SocketIoAdapter(app)); 
 
   await app.listen(3000);
 
