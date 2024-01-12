5f696bc230df57633633f345109e16f7d9111a61
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2024-01-12 16:35:23 -0300

    Update main.ts

--- api_nest/src/main.ts
+++ api_nest/src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { SocketIoAdapter } from './SocketIoAdapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Configuración específica de CORS para el servidor HTTP
   app.enableCors({
     origin: 'https://www.esencia.app',
@@ -29,17 +28,13 @@ async function bootstrap() {
 
   const httpServer = await app.listen(3000);
 
-  // Crea una instancia de IoAdapter y establece la configuración CORS
-  const ioAdapter = new IoAdapter(httpServer);
-  ioAdapter.createIOServer({
-    cors: {
-      origin: 'https://www.esencia.app',
-      methods: ['GET', 'POST'],
-      credentials: true,
-    },
-  });
+  // Pasa la instancia del servidor HTTP al adaptador de WebSockets
+  app.useWebSocketAdapter(new IoAdapter(httpServer));
 
-  app.useWebSocketAdapter(ioAdapter);
+  await app.listen(3000);
+}
+
+bootstrap();
 }
 
 bootstrap();
