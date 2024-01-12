a3c5c78354750594250490221db932caca78178b
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2024-01-12 16:49:28 -0300

    Update main.ts

--- api_nest/src/main.ts
+++ api_nest/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors()
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
