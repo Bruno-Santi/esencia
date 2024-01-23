43c547b15d4705b3015cb563bf17eb6c0b69b7a4
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2024-01-23 11:57:44 -0300

    Update apiToken.ts

--- client/src/helpers/apiToken.ts
+++ client/src/helpers/apiToken.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = `https://esencia-api.onrender.com;
+export const baseURL = `https://esencia-api.onrender.com`
 // `https://esencia-api.onrender.com`;
 const api = axios.create({
   baseURL,
