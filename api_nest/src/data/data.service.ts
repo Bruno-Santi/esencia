23b952b3d7640f9fa53ccb3178886ff0ff612d1a
Bruno Santimaria <brun0.23@outlook.com>
2024-01-26 18:35:02 -0300

    deshabilitada short da errores

--- api_nest/src/data/data.service.ts
+++ api_nest/src/data/data.service.ts
@@ -27,23 +27,22 @@ export class DataService {
 
   async getDashboardData(teamId, sprint) {
     try {
+      console.log(teamId, sprint);
+
       await this.checkTeam(teamId);
-
-      const dashboardData = await axios.get(
-        `${process.env.API_DATA}/dashboard_data?sprint=${sprint}&team_id=${teamId}`,
-      );
-      const short = await axios.post(
-        `${process.env.API_DATA}/short_recommendation?team_id=${teamId}&sprint=${sprint}`,
-      );
-
       const data2 = await axios.post(
         `${process.env.API_DATA}/get_topics?sprint=${sprint}&team_id=${teamId}`,
       );
+      const dashboardData = await axios.get(
+        `${process.env.API_DATA}/dashboard_data?sprint=${sprint}&team_id=${teamId}`,
+      );
+      // const short = await axios.post(
+      //   `${process.env.API_DATA}/short_recommendation?team_id=${teamId}&sprint=${sprint}`,
+      // );
+
       const { data } = dashboardData;
-      console.log(data2.data);
 
       console.log(data);
-      console.log(short);
 
       return {
         data,
