0f198ba09526d28eb3e817dabe2cbd39f2d278f7
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2024-03-20 12:37:59 -0300

    Update formatDate.ts

--- client/src/helpers/formatDate.ts
+++ client/src/helpers/formatDate.ts
@@ -2,5 +2,5 @@ import moment from "moment";
 
 export const formatDate = (dateString) => {
   const newDate = moment(dateString).add(1, "day");
-  return newDate.format("DD/MM/YYYY");
+  return newDate.format("MM/DD/YYYY");
 };
